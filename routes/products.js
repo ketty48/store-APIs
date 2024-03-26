4ee3972454cf3f735c31f2f9faaad1354eef222f
Magnifique UMURERWA <ketymagnifique@gmail.com>
2024-03-26 12:40:03 +0200

    product routers

--- routes/products.js
+++ routes/products.js
@@ -4,5 +4,4 @@ const {getAllProducts,getAllProductsStatic} = require('../controllers/products')
 
 router.route('/').get(getAllProducts)
 router.route('/static').get(getAllProductsStatic)
-
 module.exports=router
