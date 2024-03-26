b4f0ddb8f8e8feec9df0fd6462fe9eda087670ec
Magnifique UMURERWA <ketymagnifique@gmail.com>
2024-03-26 12:38:26 +0200

    products controllers

---
+++ README.md
@@ -0,0 +1,2 @@
+#product APIs
+this project contains APIS that can be used to search for products, to sort products, to set limits of how many products youn want to select
---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+require('express-async-errors')
+//async errors
+
+
+const express = require('express')
+const app = express()
+const connectDB=require('./db/connect')
+const productRouter=require('./routes/products')
+const notFoundMiddleware =require('./middleware/not-found')
+const errorMiddleware=require('./middleware/error-handler')
+
+app.use(express.json())
+
+
+
+app.get('/', ()=>{
+    resizeBy.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+
+})
+
+app.use('/api/v1/products', productRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+const port=process.env.PORT || 3000
+const start=async()=>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on ${port}....`));
+
+    }catch(err){
+
+    }
+}
+start()
